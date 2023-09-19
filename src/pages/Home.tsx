698eac5b7b188e82225c0c48ecdf9945f694bb70
Nur Cahyanto <nur.razdan@gmail.com>
2023-09-19 19:03:51 +0700

    global state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import Navbar from '../component/Navbar'
-import Hero from '../component/Hero'
+import React from "react";
+import Navbar from "../component/Navbar";
+import Hero from "../component/Hero";
+import { useStore } from "../store/store";
 
 const Home = () => {
+
+  const {count, increment, decrement} = useStore()
+
   return (
     <>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <div className="text-5xl w-screen flex justify-center gap-4 items-center">
+          {count}
+          <button className="btn btn-warning" onClick={increment}>tambah</button>
+          <button className="btn btn-primary" onClick={decrement}>kurang</button>
+        
+      </div>
+      <Hero />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
