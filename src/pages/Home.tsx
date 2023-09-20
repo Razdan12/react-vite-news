448b0b880bab6cbdae41b6953dd308cbf63f12e2
Nur Cahyanto <nur.razdan@gmail.com>
2023-09-20 08:39:12 +0700

    ADD LAZYLOAD

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,30 @@
+import React, {Suspense} from "react";
 import Navbar from "../component/Navbar";
-import Hero from "../component/Hero";
 import { useStore } from "../store/store";
 
-const Home = () => {
+const LazyLoad = React.lazy(() => import("../component/Hero"))
 
-  const {count, increment, decrement} = useStore()
+const Home = () => {
+  const { count, increment, decrement } = useStore();
 
   return (
     <>
       <Navbar />
       <div className="text-5xl w-screen flex justify-center gap-4 items-center">
-          {count}
-          <button className="btn btn-warning" onClick={increment}>tambah</button>
-          <button className="btn btn-primary" onClick={decrement}>kurang</button>
-        
+        {count}
+        <button className="btn btn-warning" onClick={increment}>
+          tambah
+        </button>
+        <button className="btn btn-primary" onClick={decrement}>
+          kurang
+        </button>
       </div>
-      <Hero />
+
+      <Suspense fallback={<div className="w-full flex justify-center text-6xl text-red-500">Loading...</div>}>
+        <LazyLoad/>
+      </Suspense>
+
+
     </>
   );
 };
